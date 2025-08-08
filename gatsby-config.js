68e294b47476f9350882bf2b84c3a2179e62e3da
Thasnym Ali <thasnymali@Thasnyms-MacBook-Pro.local>
2025-08-09 00:25:52 +0500

    Initial Gatsby landing page for Bilehfahi

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: "Visit Bilehfahi",
+  },
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+  ],
+}
